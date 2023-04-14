e2b65269fd48364f5d26c0a98dc37b71fb59b0ca
Alessandra-Casu <alessandracasu919@gmail.com>
2023-04-14 16:58:35 +0200

    add for

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-for(let i=0; i = 100; i++){
-    console.log(i);
+const eleGrid = document.querySelector('.grid');
+const eleDiv15 = document.querySelector('.div15');
+const eleDiv3 = document.querySelector('.div3');
+const eleDiv5 = document.querySelector('.div5');
+
+
+for(let i = 1; i <= 100; i++)
+{
+    if (i % 15 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell div15">FrizzBuzz</div>`;
+        console.log("FizzBuzz");
+    }else if (i % 3 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell div3">Frizz</div>`;
+        console.log("Fizz");
+    }else if (i % 5 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell div5">Buzz</div>`;
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+    }
+    
+
 }
+    
